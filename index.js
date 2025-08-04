cbc18af9653e41efd8cfc75adfc5f1b9cd9a5008
FizOkor <okorfavour2000@gmail.com>
2025-08-04 21:37:52 +0100

    correcto

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ app.get("/api/users", async (req, res) => {
 // Add exercise
 app.post("/api/users/:_id/exercises", async (req, res) => {
   const data = req.body;
+  // console.log("data:", data);
 
   try {
     const { username } = (await UserModel.findById(req.params._id)) || {};
@@ -62,15 +63,17 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
       description: data.description,
       duration: parseInt(data.duration),
     };
-    
+
     if (data.date && data.date.trim() !== "") {
       exerciseData.date = new Date(data.date);
+    } else {
+      exerciseData.date = new Date();
     }
 
     // Now create the document
     const newExercise = await ExerciseModel.create(exerciseData);
 
-    console.log("testingggRxercise:", newExercise);
+    // console.log("testingggRxercise:", newExercise);
     res.json({
       _id: newExercise.userId,
       username,
@@ -87,37 +90,73 @@ app.get("/api/users/:_id/logs", async (req, res) => {
   const userId = req.params._id;
   const { username } = (await UserModel.findById(req.params._id)) || {};
   if (!username) return res.status(400).json({ error: "User not found" });
+  console.log("id:", userId, "username:", username);
 
-  if (req.query) {
-    const from = new Date(req.query.from);
-    const to = new Date(req.query.to);
-    const limit = req.query.limit;
+  if (!(JSON.stringify(req.query) === "{}")) {
+    console.log("query:", req.query);
 
-    const logs = await ExerciseModel.find({
+    const from = new Date(req.query.from) || null;
+    const to = new Date(req.query.to) || null;
+    const limit = req.query.limit ? parseInt(req.query.limit) : null;
+
+    console.log("query data:", from, to, limit);
+
+    let logs;
+
+    if(req.query.from || req.query.to) {
+      logs = await ExerciseModel.find({
+      userId,
       date: { $gte: from, $lte: to },
-    }).limit(limit);
+    });
+    } else {
+      logs = await ExerciseModel.find({userId});
+    }
 
-    const count = response.length;
+    if (req.query.limit) {
+      logs = logs.splice(0, limit);
+    }
+
+    // console.log('logs:', logs)
+
+    const count = logs.length;
+
+    console.log("logs count:", count);
 
     const response = {
       username,
       count,
-      _id,
+      _id: userId,
       log: logs.map((entry) => {
         return {
-          descrition: entry.description,
+          description: entry.description,
           duration: Number(entry.duration),
-          date: entry.date.toString().toDateString(),
+          date: entry.date.toDateString(),
         };
       }),
     };
+    console.log("response:", response.username, response.count);
 
     return res.json(response);
   }
 
   const allExercises = await ExerciseModel.find({ userId });
+  // console.log("allExercises:", allExercises);
 
-  res.send(allExercises);
+  const response = {
+    username,
+    count: allExercises.length,
+    _id: userId,
+    log: allExercises.map((entry) => {
+      return {
+        description: entry.description,
+        duration: Number(entry.duration),
+        date: entry.date.toDateString(),
+      };
+    }),
+  };
+
+  // console.log(response.log)
+  res.send(response);
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
